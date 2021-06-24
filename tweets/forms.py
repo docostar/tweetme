c25e4a7653768ec22d4c064070db1a92b210503e
test-git <dholariyarahul@gmail.com>
2021-06-24 18:01:09 +0530

    33. User&Tweets

--- tweets/forms.py
+++ tweets/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from django.db.models.base import Model
 from django.forms import fields
 from .models import Tweet
-MAX_TWEET_LENGTH=240
+from django.conf import settings
 
+MAX_TWEET_LENGTH=240
+User = settings.AUTH_USER_MODEL
 class TweetForm(forms.ModelForm):
     class  Meta:
         model=Tweet
@@ -12,5 +14,5 @@ class TweetForm(forms.ModelForm):
     def clean_content(self):
         content=self.cleaned_data.get("content")
         if len(content) > MAX_TWEET_LENGTH:
-            raise Form.ValidationError("This Tweet is too long")
+            raise forms.ValidationError("This Tweet is too long")
         return content
--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,14 @@
 import random
+import django
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Tweet(models.Model):
     id=models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can many tweets
     content=models.TextField(blank=True,null=True)
     image=models.FileField(upload_to='images/',blank=True,null=True)
     #image=models.ImageField(blank=True,upload_to='images/',null=True)
