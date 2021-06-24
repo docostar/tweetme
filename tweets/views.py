b09ad090656fbd03be5f221c6a0b9420c708c47f
test-git <dholariyarahul@gmail.com>
2021-06-24 10:26:11 +0530

    like button added and render it by jS

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404,JsonResponse
 from .models import Tweet
+import random
 # Create your views here.
 
 
@@ -11,7 +12,7 @@ def home_view(request,*args,**kwargs):
 
 def tweet_list_view(request,*args, **kwargs):
     qs=Tweet.objects.all()
-    tweets_list=[{"id":x.id,"content":x.content} for x in qs]
+    tweets_list=[{"id":x.id,"content":x.content,"likes":random.randint(0,50)} for x in qs]
     data={
         "isUser":False,
         "response": tweets_list
