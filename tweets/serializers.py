4bd1f933eee62c54e9543506e593f2a6c08cfa05
test-git <dholariyarahul@gmail.com>
2021-06-27 11:20:13 +0530

    45. Javascript Tweet Action Handler

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -6,10 +6,14 @@ MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
 WEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 
 class TweetSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Tweet
-        fields = ['content']
+        fields = ['id','content','likes']
     
+    def get_likes(self, obj):
+        return obj.likes.count()
+
     def validate_content(self, value):
         if len(value) > MAX_TWEET_LENGTH:
             raise serializers.ValidationError("This tweet is too long")
