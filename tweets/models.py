bddcb034f1c6f0a3958e171a3d56936662cb6bbe
test-git <dholariyarahul@gmail.com>
2021-06-27 18:45:25 +0530

    50. Django Module Complete Test implimented

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,47 @@
+# import random
+# from django.conf import settings
+# from django.db import models
+
+# User = settings.AUTH_USER_MODEL
+
+# class TweetLike(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+#     timestamp = models.DateTimeField(auto_now_add=True)
+
+# class Tweet(models.Model):
+#     # Maps to SQL data
+#     # id = models.AutoField(primary_key=True)
+#     parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can many tweets
+#     likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
+#     content = models.TextField(blank=True, null=True)
+#     image = models.FileField(upload_to='images/', blank=True, null=True)
+#     timestamp = models.DateTimeField(auto_now_add=True)
+
+#     # def __str__(self):
+#     #     return self.content
+    
+#     class Meta:
+#         ordering = ['-id']
+    
+#     @property
+#     def is_retweet(self):
+#         return self.parent != None
+    
+#     def serialize(self):
+#         '''
+#         Feel free to delete!
+#         '''
+#         return {
+#             "id": self.id,
+#             "content": self.content,
+#             "likes": random.randint(0, 200)
+#         }
+
 import random
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
@@ -9,26 +50,32 @@ class TweetLike(models.Model):
     tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    
-# Create your models here.
 class Tweet(models.Model):
-    id=models.AutoField(primary_key=True)
+    # Maps to SQL data
+    # id = models.AutoField(primary_key=True)
+    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can many tweets
-    likes=models.ManyToManyField(User,related_name='tweet_user',blank=True, through=TweetLike)
-    content=models.TextField(blank=True,null=True)
-    image=models.FileField(upload_to='images/',blank=True,null=True)
-    #image=models.ImageField(blank=True,upload_to='images/',null=True)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering=['-id']
-
     # def __str__(self):
-        #  return self.content
-
+    #     return self.content
+    
+    class Meta:
+        ordering = ['-id']
+    
+    @property
+    def is_retweet(self):
+        return self.parent != None
+    
     def serialize(self):
-        return{
-            "id":self.id,
+        '''
+        Feel free to delete!
+        '''
+        return {
+            "id": self.id,
             "content": self.content,
-            "likes": random.randint(0,100)
+            "likes": random.randint(0, 200)
         }
--- tweets/urls.py
+++ tweets/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path, include
-from .views import home_view,tweet_detail_view,tweet_list_view,tweet_create_view,tweet_delete_view,tweet_action_view
+from django.urls import path
 
+from .views import (
+    home_view, 
+    tweet_action_view,
+    tweet_delete_view,
+    tweet_detail_view, 
+    tweet_list_view,
+    tweet_create_view,
+)
+'''
+CLIENT
+Base ENDPOINT /api/tweets/
+'''
 urlpatterns = [
-    path("",home_view,name="home"),
-    path("create-tweet",tweet_create_view,name="create"),
-    path("tweet/<int:tweet_id>",tweet_detail_view),
-    path("tweets",tweet_list_view),
-    path('api/tweets/action', tweet_action_view),
-    path("api/tweets/<int:tweet_id>/delete",tweet_delete_view)
+    path('', tweet_list_view),
+    path('action/', tweet_action_view),
+    path('create/', tweet_create_view),
+    path('<int:tweet_id>/', tweet_detail_view),
+    path('<int:tweet_id>/delete/', tweet_delete_view),
 ]
